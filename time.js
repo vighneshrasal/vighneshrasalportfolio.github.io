32cf060e0fa64a3ce7e65c718f70881ceffb3eda
Vighnesh Rasal <56250868+vighneshrasal@users.noreply.github.com>
2023-09-05 07:28:10 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,4 +1,4 @@
-let warning = alert("This webpage is made for Desktops and Laptops, if you're using a cell phone you need to switch");
+let warning = alert("This webpage is made for Desktops and Laptops, if you're using a cell phone I request you to Switch");
 let userName = prompt("ENTER YOUR NAME");
 let date = new Date();
 const options = {weekday: 'short', year : 'numeric', month: 'short', day: 'numeric'};
@@ -9,3 +9,10 @@ if(userName != ""){
 
 fullDate = date.toLocaleDateString(undefined, options);
 document.getElementById('for-date').innerHTML = fullDate;
+
+console.log("We're no strangers to love");
+console.log("You know the rules and so do I (do I)");
+console.log("A full commitment's what I'm thinking of");
+console.log("You wouldn't get this from any other guy");
+console.log("I just wanna tell you how I'm feeling");
+console.log("Gotta make you understand");
