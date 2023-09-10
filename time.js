2ade50eccb1c095ee0572088504433449d78d036
Vighnesh Rasal <56250868+vighneshrasal@users.noreply.github.com>
2023-09-10 13:36:31 +0530

    Update time.js

--- time.js
+++ time.js
@@ -11,16 +11,3 @@ if(userName != ""){
 
 fullDate = date.toLocaleDateString(undefined, options);
 document.getElementById('for-date').innerHTML = fullDate;
-
-console.log("We're no strangers to love");
-console.log("You know the rules and so do I (do I)");
-console.log("A full commitment's what I'm thinking of");
-console.log("You wouldn't get this from any other guy");
-console.log("I just wanna tell you how I'm feeling");
-console.log("Gotta make you understand");
-console.log("Never gonna give you up");
-console.log("Never gonna let you down");
-console.log("Never gonna run around and desert you");
-console.log("Never gonna make you cry");
-console.log("Never gonna say goodbye");
-console.log("Never gonna tell a lie and hurt you");
